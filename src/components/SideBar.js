20c18cf397819c9bb16c63268d2a746d89297c6f
Boucqueau <gyboucqueau@hotmail.com>
2020-04-13 16:30:49 +0200

    creation of SideBar component

---
+++ src/components/SideBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function SideBar() {
+    const styles = {
+        sideContainer: {
+            backgroundColor: 'blue',
+            height: '100vh',
+            width: '38%',
+            margin: '0px',
+        },
+    }
+    return (
+        <div style={styles.sideContainer}>
+        </div>
+    );
+}
