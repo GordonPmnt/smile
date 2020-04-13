b76a27559d4ddb050aa70066b4cfc2a32f62ff59
Boucqueau <gyboucqueau@hotmail.com>
2020-04-13 19:26:18 +0200

    adding SelectedJoke component

--- src/components/OpponentCam.js
+++ src/components/OpponentCam.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SelectedJoke from "./SelectedJoke";
 
 export default function OpponentCam() {
     const styles = {
@@ -7,9 +8,17 @@ export default function OpponentCam() {
             height: '100vh',
             width: '70%',
             margin: '0px',
+            position: 'relative',
+            zIndex: '2',
         },
+
     }
     return (
-        <img src="https://image.freepik.com/free-photo/young-bearded-crazy-man-laughing-looking-happy-positive-surprised-realizing-great-idea-pointing-lateral-copyspace-against-flat-color_1194-35140.jpg" style={styles.opponentCamBox} />
+        <div  style={styles.opponentCamBox}>
+            <div>
+                <SelectedJoke />
+            </div>
+            <img src="https://image.freepik.com/free-photo/young-bearded-crazy-man-laughing-looking-happy-positive-surprised-realizing-great-idea-pointing-lateral-copyspace-against-flat-color_1194-35140.jpg" display="block" width="100%" height="100%" />
+        </div>
     );
 }
