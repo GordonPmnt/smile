2fb95b1f38e63928b5986d987c93df5633f40811
Guillaume-Kreit <Guillaume.kreit@outllook.com>
2020-04-13 16:26:31 +0200

    Add H1

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -8,8 +8,15 @@ export default function MainPage() {
       height: '100vh'
     },
 
+    title: {
+      color: 'red',
+      marginLeft: '40%',
+      paddingTop: '10%',
+      fontSize: '80px'
+    },
+
     start: {
-      marginTop : '25%',
+      marginTop : '5%',
       marginLeft : '50%',
       backgroundColor: '#008CBA',
       border: 'none',
@@ -24,6 +31,7 @@ export default function MainPage() {
 
   return (
   <div style={styles.container}>
+    <h1 style={styles.title}>Party time !</h1>
     <button style={styles.start}>start</button>
   </div>
   );
