5932f8412d8309f03e28d3b9362ef1e6cc39a53c
Boucqueau <gyboucqueau@hotmail.com>
2020-04-13 16:44:21 +0200

    creation OpponentCam component

---
+++ src/components/OpponentCam.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function OpponentCam() {
+    const styles = {
+        opponentCamBox: {
+            backgroundColor: 'green',
+            height: '100vh',
+            width: '70%',
+            margin: '0px',
+        },
+    }
+    return (
+        <div style={styles.opponentCamBox}>
+        </div>
+    );
+}
