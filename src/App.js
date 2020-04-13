02cb4cf6983706280fd748579ec1eb97adf9516a
Guillaume-Kreit <Guillaume.kreit@outlook.com>
2020-04-13 10:45:02 +0200

    creation main page v1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import MainPage from './Components/MainPage';
 
 
 function App() {
   return (
-    <>
-      <h1>Pouet</h1>
-    </>
+    <div className="App">
+      <MainPage />
+    </div>
   );
 }
 
---
+++ src/Components/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MainPage.css';
+
+function MainPage() {
+  return (
+    <div class="green">
+      <p>MainPage</p>
+    </div>
+  );
+  }
+
+  export default MainPage
+  ;
