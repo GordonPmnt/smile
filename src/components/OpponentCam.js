115da7dcf0d9517bacbf1fab4c52fce3147267dd
Gordon Preumont <preumontgr@gmail.com>
2020-04-13 20:20:28 +0200

    change opponent cam picture

--- src/components/OpponentCam.js
+++ src/components/OpponentCam.js
@@ -3,13 +3,18 @@ import React from "react";
 export default function OpponentCam() {
     const styles = {
         opponentCamBox: {
-            backgroundColor: 'green',
             height: '100vh',
             width: '70%',
             margin: '0px',
         },
     }
+    const picture = require('../img/fakecam.png')
     return (
-        <img src="https://image.freepik.com/free-photo/young-bearded-crazy-man-laughing-looking-happy-positive-surprised-realizing-great-idea-pointing-lateral-copyspace-against-flat-color_1194-35140.jpg" style={styles.opponentCamBox} />
+        <div style={styles.opponentCamBox}>
+            <img 
+                src={picture}
+                alt="fake webcam"
+            />
+        </div>
     );
 }
