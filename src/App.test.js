6621c3e101401f3f9232183b2530c16e83fc9a6e
Gordon Preumont <preumontgr@gmail.com>
2020-04-13 10:24:20 +0200

    Writte test for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { create, act } from "react-test-renderer";
 import App from './App';
+import MainPage from './components/MainPage';
 
 
+describe("App component", () => {
+  it("renders MainPage", () => {
+    let root;
+    act(() => {
+      root = create(<App />);
+    });
+    const { rendered } = root.toTree()
+    expect(rendered.type).toBe(MainPage)
+  });
+});
