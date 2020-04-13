8545945cf275b5f5bb3ee87c7252c3a32bedb0af
Guillaume-Kreit <Guillaume.kreit@outllook.com>
2020-04-13 16:14:53 +0200

    Button style mainPage

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -7,12 +7,24 @@ export default function MainPage() {
       width : '100%',
       height: '100vh'
     },
+
+    start: {
+      marginTop : '25%',
+      marginLeft : '50%',
+      backgroundColor: '#008CBA',
+      border: 'none',
+      color: 'white',
+      padding: '15px 32px',
+      textAlign: 'center',
+      textDecoration: 'none',
+      fontSize: '16px'
+    },
+
   }
 
   return (
   <div style={styles.container}>
-    <p >MainPage</p>
-    <button>start</button>
+    <button style={styles.start}>start</button>
   </div>
   );
 }
