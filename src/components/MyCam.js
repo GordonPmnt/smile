7203164b8150afeac137ae938d711c499ce73be5
Guillaume-Kreit <Guillaume.kreit@outllook.com>
2020-04-16 12:13:15 +0200

    Modify Size Webcam

--- src/components/MyCam.js
+++ src/components/MyCam.js
@@ -3,7 +3,14 @@ import Webcam from "react-webcam";
  
 const MyCam = ({ mirrored }) => {
     return (
-        <Webcam mirrored={mirrored} />
+        <Webcam mirrored={mirrored} 
+        
+        
+        height={250}
+        width={470}
+        
+        />
+
     )
 };
 
