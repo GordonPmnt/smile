79dfbec9bf1215c8044cc8de78dfe387da0a4ebe
Gordon Preumont <preumontgr@gmail.com>
2020-04-17 10:28:24 +0200

    indentation WebCam

--- src/components/MyCam.js
+++ src/components/MyCam.js
@@ -2,15 +2,13 @@ import React from "react";
 import Webcam from "react-webcam";
  
 const MyCam = ({ mirrored }) => {
+    
     return (
-        <Webcam mirrored={mirrored} 
-        
-        
-        height={250}
-        width={470}
-        
+        <Webcam 
+            mirrored={mirrored} 
+            height={250}
+            width={470}
         />
-
     )
 };
 
