07fdd020276c03ed1c84d4b4b8a8c0cf998ace1f
Guillaume-Kreit <Guillaume.kreit@outlook.com>
2020-04-20 18:05:40 +0200

    access API

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,5 +1,30 @@
 import React from "react";
 
+
+
+
+
+// import axios from 'axios';
+
+// const config = {
+//     headers: { Authorization: `Bearer ${token}` }
+// };
+// const bodyParameters = {
+//    key: "value"
+// };
+// Axios.post( 
+//   'http://localhost:8000/api/v1/get_token_payloads',
+//   bodyParameters,
+//   config
+// ).then(console.log).catch(console.log);
+
+
+
+
+
+
+
+
 function Deck() {
     const styles = {
         card: {
@@ -18,3 +43,10 @@ function Deck() {
 }
 
 export default Deck;
+
+
+
+
+
+
+
--- src/components/DecksList.js
+++ src/components/DecksList.js
@@ -1,25 +1,41 @@
-import React from "react";
+import React, {Component} from "react";
 import Deck from './Deck';
+import axios from 'axios';
 
-function DecksList() {
-    const styles = {
-        container: {
-            width: '100%',
-            backgroundColor: 'red',
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'space-around',
-        },
+const styles = {
+    container: {
+        width: '100%',
+        backgroundColor: 'red',
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+    },
+}
+
+
+ class DecksList extends Component {
+     componentDidMount(){
+
+        axios
+      .get('/api/joke/1', {
+          method: 'get',
+          headers: { Authorization: 'wNCRy1cy4Uu.sPBmUEFcY5g991jBK7TD-OzSO.83-iar0fDBIYBKG6qFe-N.wH2W' }
+      })
+      .then(res => {
+          console.log(res.data)
+      });
+}
+    
+    
+    render(){
+        return (
+            <div style={styles.container}>
+                <Deck/>
+                <Deck/>
+                <Deck/>
+            </div>
+        );
     }
-
-    return (
-        <div style={styles.container}>
-            <Deck/>
-            <Deck/>
-            <Deck/>
-            <Deck/>
-        </div>
-    );
 }
 
 export default DecksList;
