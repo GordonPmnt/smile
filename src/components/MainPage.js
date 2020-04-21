a29039a440ca7990a6e19ca770b52b7207836ffb
Gordon Preumont <preumontgr@gmail.com>
2020-04-21 21:54:22 +0200

    main page Css

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import theme from './utils/theme';
 import { Link } from "react-router-dom";
 
 
 export default function MainPage() {
   const styles = {
     container: {
-      width : '100%',
+      display: 'flex',
+      width : '100vw',
       height: '100vh',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      justifyItems: 'center',
+      alignContent: 'center',
     },
 
     title: {
-      marginLeft: '40%',
-      fontSize: '80px',
+      textAlign: 'center',
+      color: theme.colors.pink,
     },
 
     start: {
+      display: 'flex',
       marginTop : '5%',
       marginLeft : '50%',
       padding: '15px 32px',
@@ -28,7 +36,12 @@ export default function MainPage() {
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>Party time !</h1>
-      <Link to="/GameRoom" style={styles.start}>Start</Link>
+      <Link 
+        to="/GameRoom" 
+        style={styles.start}
+      >
+        Start
+      </Link>
     </div>
   );
 }
