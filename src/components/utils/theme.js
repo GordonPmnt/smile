0a597b1ff4f45318e93a3a03162cd5097c85269f
Gordon Preumont <preumontgr@gmail.com>
2020-04-21 14:50:59 +0200

    creation of theme

---
+++ src/components/utils/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+    colors: {
+        yellow: '#EEE577',
+        lightYellow: '#EFEEC5',
+        red: '#EF4141',
+        lightRed: '#E5B2B2',
+        cyan: '#74FCFD',
+        lightCyan: '#BFFAFE',
+        pink: '#DA4CB0',
+        lightPink: '#F2E4EF',
+        green: '#5FCE43'
+    }
+}
+
+export default theme;
