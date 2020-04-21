1097db6a11f84724f2b98b7ba9fad0a262c4a19b
Gordon Preumont <preumontgr@gmail.com>
2020-04-21 12:23:59 +0200

    rewrite class Decklist

--- src/components/DecksList.js
+++ src/components/DecksList.js
@@ -3,37 +3,39 @@ import Deck from './Deck';
 import axios from 'axios';
 
 
-const styles = {
-    container: {
-        width: '100%',
-        backgroundColor: 'red',
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-    },
-}
-
-
  class DecksList extends Component {
-     componentDidMount(){
+    styles = {
+        container: {
+            width: '100%',
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-around',
+        },
+    }
 
+    componentDidMount(){
         axios
-      .get('/api/joke/1', {
-          method: 'get',
-          headers: { Authorization: 'FW6CstM9yETDGYTEqdL-R.4fNoGEUCRHW0SvHOGXo2YpK2j-4th5JY3pTT_qDtWX' }
-      })
-      .then(res => {
-          console.log(res.data)
-      });
-}
-
+        .get('/api/joke/1', {
+            method: 'get',
+            headers: { 
+                Authorization: 'FW6CstM9yETDGYTEqdL-R.4fNoGEUCRHW0SvHOGXo2YpK2j-4th5JY3pTT_qDtWX' 
+            }
+        })
+        .then(res => 
+            console.log(res.data)
+        )
+        .catch(err => 
+            console.log(err.message)
+        )
+    }
     
     render(){
         return (
-            <div style={styles.container}>
-                <Deck/>
-                <Deck/>
-                <Deck/>
+            <div style={this.styles.container}>
+                <Deck />
+                <Deck />
+                <Deck />
+                <Deck />
             </div>
         );
     }
