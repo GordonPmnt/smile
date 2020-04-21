7503d043ad6c8373ee59b32c87d0d753a50a6829
Gordon Preumont <preumontgr@gmail.com>
2020-04-21 15:28:54 +0200

    adjust opponent cam style

--- src/components/OpponentCam.js
+++ src/components/OpponentCam.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SelectedJoke from "./SelectedJoke";
 import ScreenshotButton from './subComponents/ScreenshotButton';
+import theme from './utils/theme';
 
 
 export default function OpponentCam({ toggleActivity }) {
@@ -10,24 +11,30 @@ export default function OpponentCam({ toggleActivity }) {
             width: '70vw',
             margin: '0',
             display: 'flex',
-            justifyContent: 'center'
+            justifyContent: 'center',
         },
-        webCam: {
+        OpponentInterface: {
             margin: '3vh',
             height: '100%',
+            width: '100%',
+            border: `solid ${theme.colors.yellow} 5px`, // This should come from selectedJoke
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        },
+        webcam: {
+            height: '100%',
+            width: '100%'
         }
-
     }
     const picture = require('../img/fakecam.png')
     return (
         <div  style={styles.container}>
-            <SelectedJoke toggleActivity={toggleActivity} />
-            <img
-                style={styles.webCam}
-                src={picture}
-                alt="fake webcam"
-            />
-            <ScreenshotButton toggleActivity={toggleActivity} />
+            <div style={styles.OpponentInterface}>
+                <img src={picture} alt="fake webcam" style={styles.webcam}/>
+                <SelectedJoke toggleActivity={toggleActivity} />
+                <ScreenshotButton toggleActivity={toggleActivity} />
+            </div>
         </div>
     );
 }
