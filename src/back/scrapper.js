8362d7a0daa9d30b5d63102535c3377473efc879
Gordon Preumont <preumontgr@gmail.com>
2020-04-23 21:41:10 +0200

    scrapper filtering

--- src/back/scrapper.js
+++ src/back/scrapper.js
@@ -2,14 +2,38 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const fs = require('fs');
 
-const url = "https://jokes-de-papa.com/blagues-courtes/blagues-de-beauf/?quotes_page=1"
+const url = "https://jokes-de-papa.com/blagues-courtes/blagues-de-beauf/"
+const param = "?quotes_page=1"
 
 JSDOM.fromURL(url)
     .then(dom => {
         const body = dom.window.document.body;
         const quotes = body.getElementsByTagName('blockquote');
-        const jokes = [...quotes].map(quote => quote.textContent);
 
+        const jokes = [...quotes]
+            .map(
+                quote => quote.textContent
+            )
+            .filter(
+                quote => quote.slice(0, 19) !== 'Vous aimez les jeux'
+            )
+            .map(
+                quote => {
+                    let joke = ''
+                    for(let j=0; j<quote.length; j++) {
+                        if(quote.slice(j, j+8).includes('— @Jokes')) {
+                            break
+                        }
+                        joke += quote[j];
+                    }
+                    return joke
+                }
+            )
+            .filter(
+                quote => quote.slice(0, 4) !== 'eval'
+            )
+        ;
+    
         const data = jokes.map(
             (joke, id) => {
                 return {
@@ -17,7 +41,7 @@ JSDOM.fromURL(url)
                     joke,
                 };
             }
-        )
+        );
 
         let jsonString = JSON.stringify(data);
         fs.writeFileSync(
