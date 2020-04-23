9221bd068df7f745b63d41f9c61a4df01a0e0aa4
Gordon Preumont <preumontgr@gmail.com>
2020-04-23 23:01:37 +0200

    scrapper loop

--- src/back/scrapper.js
+++ src/back/scrapper.js
@@ -2,57 +2,70 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const fs = require('fs');
 
-const url = "https://jokes-de-papa.com/blagues-courtes/blagues-de-beauf/"
-const param = "?quotes_page=1"
+const category = "blagues-courtes/blagues-de-beauf/"
+const pages = 35
 
-JSDOM.fromURL(url)
-    .then(dom => {
-        const body = dom.window.document.body;
-        const quotes = body.getElementsByTagName('blockquote');
+const scrapper = async (category, pages) => {
+    let data = [];
+    let id = 0;
 
-        const jokes = [...quotes]
-            .map(
-                quote => quote.textContent
-            )
-            .filter(
-                quote => quote.slice(0, 19) !== 'Vous aimez les jeux'
-            )
-            .map(
-                quote => {
-                    let joke = ''
-                    for(let j=0; j<quote.length; j++) {
-                        if(quote.slice(j, j+8).includes('— @Jokes')) {
-                            break
+    for(let page=1; page<=pages; page++) {
+        await JSDOM.fromURL(`https://jokes-de-papa.com/${category}?quotes_page=${page}`)
+            .then(dom => {
+                const body = dom.window.document.body;
+                const quotes = body.getElementsByTagName('blockquote');
+    
+                const jokes = [...quotes]
+                    .map(
+                        quote => quote.textContent
+                    )
+                    .filter(
+                        quote => quote.slice(0, 19) !== 'Vous aimez les jeux'
+                    )
+                    .map(
+                        quote => {
+                            let joke = ''
+                            for(let j=0; j<quote.length; j++) {
+                                if(quote.slice(j, j+8).includes('— @Jokes')) {
+                                    break
+                                }
+                                joke += quote[j];
+                            }
+                            return joke
                         }
-                        joke += quote[j];
+                    )
+                    .filter(
+                        quote => quote.slice(0, 4) !== 'eval'
+                    )
+                ;
+            
+                data.push(...jokes.map(
+                    joke => {
+                        id += 1;
+                        return {
+                            id,
+                            joke,
+                        };
                     }
-                    return joke
-                }
-            )
-            .filter(
-                quote => quote.slice(0, 4) !== 'eval'
+                ));
+    
+            })
+            .catch(
+                err => console.log(err.message)
             )
         ;
-    
-        const data = jokes.map(
-            (joke, id) => {
-                return {
-                    id,
-                    joke,
-                };
-            }
-        );
+    }
+    return data;
+};
 
-        let jsonString = JSON.stringify(data);
+scrapper(category, pages).then(
+    data => {
         fs.writeFileSync(
             './src/back/data/redneckJokes.json', 
-            jsonString, 
+            JSON.stringify(data), 
             err => { if (err) throw err }
-        );
-        
+        )
         console.log('Redneck jokes generated')
+    }
+);
 
-    })
-    .catch(
-        err => console.log(err.message)
-    )
