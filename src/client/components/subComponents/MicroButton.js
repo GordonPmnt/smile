e4a19d8498de5a37880e8b0ef9977243c1145d8e
Guillaume-Kreit <Guillaume.kreit@outllook.com>
2020-04-27 22:30:43 +0200

    Conversion MicroButton function for class

--- src/client/components/subComponents/MicroButton.js
+++ src/client/components/subComponents/MicroButton.js
@@ -1,58 +1,41 @@
 import React from 'react';
 
-const MicroButton = () => {
-    const styles = {
-        button: {
-            backgroundColor : 'white',
-            boxShadow: '0px 10px 10px rgba(0, 0, 0, 0.50)',
-            position: 'absolute',
-            marginLeft: '11%',
-            marginTop: '-4%',
-            cursor: 'pointer',
-            height: '3em',
-            width: '3em',
-            borderRadius: '100%',
-        },
-        text: {
-            display: 'flex',
-            justifyContent: 'center',
+class MicroButton extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state= {
+
+            button: {
+                backgroundColor : 'white',
+                boxShadow: '0px 10px 10px rgba(0, 0, 0, 0.50)',
+                position: 'absolute',
+                marginLeft: '11%',
+                marginTop: '-4%',
+                cursor: 'pointer',
+                height: '3em',
+                width: '3em',
+                borderRadius: '100%',
+            },
+
+            text: {
+                display: 'flex',
+                justifyContent: 'center',
+            }
         }
     }
-    
-    return (
-        <div style={styles.button}>
-            <p style={styles.text}>MICRO</p>
-        </div>
-    )
+
+    render() {
+
+        console.log("hello");
+
+        return (
+            <div style={this.state.button}>
+                <p style={this.state.text}>MICRO</p>
+            </div>
+        );
+    }
 }
 
-// class MicroButton extends React.Component {
-//    render() {
-
-    // const styles = {
-    //     button: {
-    //         backgroundColor : 'white',
-    //         boxShadow: '0px 10px 10px rgba(0, 0, 0, 0.50)',
-    //         position: 'absolute',
-    //         marginLeft: '11%',
-    //         marginTop: '-4%',
-    //         cursor: 'pointer',
-    //         height: '3em',
-    //         width: '3em',
-    //         borderRadius: '100%',
-    //     },
-    //     text: {
-    //         display: 'flex',
-    //         justifyContent: 'center',
-    //     }
-    // }
-
-//       return (
-//         <button onClick={this.audio}>
-//           {this.state.count}
-//         </button>
-//       );
-//     }
-//    }
 
 export default MicroButton;
