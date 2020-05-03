65e3ebe87300c2dfd2d5d1dae58db2188c679166
Gordon Preumont <preumontgr@gmail.com>
2020-05-03 20:43:42 +0200

    aws server

--- src/client/components/GameRoom.js
+++ src/client/components/GameRoom.js
@@ -32,7 +32,7 @@ class GameRoom extends React.Component {
     componentDidMount = () => {
         if(this.props.player) {
             const { player } = this.props;
-            this.socketEndpoint = `${config.socket.local}?name=${this.props.player}`;
+            this.socketEndpoint = `${config.socket.aws}?name=${this.props.player}`;
             this.socket = socketIOClient(this.socketEndpoint);
             this.socket.on(
                 'player status',
