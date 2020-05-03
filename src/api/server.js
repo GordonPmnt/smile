d7fb6a6fd06e4fbed0cc6f95b4383eec25126024
Gordon Preumont <preumontgr@gmail.com>
2020-05-03 15:13:21 +0200

    adapt server to received WebRTC calls

--- src/api/server.js
+++ src/api/server.js
@@ -10,29 +10,43 @@ const gameroom = {}
 io.on('connection', (socket) => {
     
     let player = socket.request._query.name;
-    gameroom[player] = true;
-    console.log('GAMEROOM:', gameroom)
-    
-    io.emit('room', Object.keys(gameroom).length)
+    gameroom[player] = socket.id;
+    console.log(gameroom)
+
+    io.emit('room-size', Object.keys(gameroom).length)
+    io.emit('update-user-list', gameroom)
     
     let id = 0;
     socket.on('chat message', (msg) => {
+        msg.socketId = socket.id
         msg.id = id;
-        console.log(msg.id + '//' + msg.sender + '//' + msg.message);
         io.emit('chat message', msg);
         id +=1;
     });
 
     socket.on('player status', playerStatus => {
-        const { player, status } = playerStatus
-        console.log(`${player} is active: ${status}`)
         io.emit('player status', playerStatus)
+    });
+
+    socket.on("call-user", data => {
+        socket.to(data.to).emit("call-made", {
+          offer: data.offer,
+          socket: socket.id
+        });
+    });
+
+    socket.on("make-answer", data => {
+        socket.to(data.to).emit("answer-made", {
+          socket: socket.id,
+          answer: data.answer
+        });
     });
 
     socket.on("disconnect", () => {
         console.log(`${player}'s client disconnected`);
         delete gameroom[player];
-        console.log('GAMEROOM:', gameroom)
+        console.log(gameroom)
+        io.emit('update-user-list', gameroom)
     });
     
 });
