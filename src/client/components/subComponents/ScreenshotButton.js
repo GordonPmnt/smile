199a9f9e2c9d1fcf998c5114e662dd8111d421ff
Gordon Preumont <preumontgr@gmail.com>
2020-05-07 11:58:29 +0200

    hover effect on screenshot button

--- src/client/components/subComponents/ScreenshotButton.js
+++ src/client/components/subComponents/ScreenshotButton.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
+
+const useStyles = makeStyles({
+    button: {
+        borderRadius: '100%',
+        position: 'absolute',
+        bottom: '10%',
+        height: '5em',
+        boxShadow: '0px 10px 10px rgba(0, 0, 0, 0.05)',
+        cursor: 'pointer',
+        border: 'solid 3px',
+        transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
+            "&:hover": {
+            transform: "scale(1.04)",
+            boxShadow: "0 4px 20px 0 rgba(0,0,0,0.12)",
+        },
+    },
+})
 
 const ScreenshotButton = ({ handleEndOfturn, theme, userIsActive }) => {
     const styles = {
@@ -7,16 +25,8 @@ const ScreenshotButton = ({ handleEndOfturn, theme, userIsActive }) => {
             display: 'flex',
             justifyContent: 'center',
         },
-        button: {
-            borderRadius: '100%',
-            position: 'absolute',
-            bottom: '10%',
-            height: '5em',
-            boxShadow: '0px 10px 10px rgba(0, 0, 0, 0.50)',
-            cursor: 'pointer',
-            border: 'solid 3px',
-        }
     }
+    const classes = useStyles();
     
     return (
         <div style={styles.container}>
@@ -24,7 +34,8 @@ const ScreenshotButton = ({ handleEndOfturn, theme, userIsActive }) => {
                 src={require('../../img/cam-moustache.png')}
                 alt="Screenshot button"
                 onClick={userIsActive ? () => handleEndOfturn() : false} //here: should replace false by screenshot only 
-                style={{...styles.button, ...theme.borderColor}}
+                style={{...theme.borderColor}}
+                className={classes.button}
             />
         </div>
     )
