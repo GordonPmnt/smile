2eba7941d709b373ec892bacb24ae7d6e0529fc1
Guillaume-Kreit <Guillaume.kreit@outllook.com>
2020-05-12 11:57:55 +0200

    Function capture

--- src/client/components/subComponents/ScreenShot.js
+++ src/client/components/subComponents/ScreenShot.js
@@ -1,18 +1,25 @@
 import React from "react";
 
 
-const ScreenShot = () => {
+const ScreenShot = (props) => {
     const styles = {
         container: {
             width: '30%',
             paddingTop : '30px',
             display: 'inline-block',
             textAlign: 'center',
+            backgroundColor: 'red',
         },
     }
 
+    console.log(props.myCaptureImg);
+    console.log(props.opponentCaptureImg);
+
     return (
-        <div style={styles.container}></div>
+        <div style={styles.container}>
+            <img src={`data:image/jpeg;base64,${props.myCaptureImg}`} />
+            <img src={props.opponentCaptureImg}/>
+        </div>
         );
 }
 
