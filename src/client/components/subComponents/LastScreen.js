610203d364fb22400bbcde27a1b747dcea4f3454
Guillaume-Kreit <Guillaume.kreit@outllook.com>
2020-05-13 18:27:10 +0200

    style for style

--- src/client/components/subComponents/LastScreen.js
+++ src/client/components/subComponents/LastScreen.js
@@ -1,20 +1,19 @@
 import React from "react";
 
-
 const LastScreen = ({ winnerCapture, looserCapture, displayLastscreen }) => {
     const styles = {
-        winnerCapture: {
-            border : '75px solid white',
+        looserCapture: {
+            border : '5vh solid white',
             boxShadow: '6px 6px 6px black',
             height : '35vw',
             width : 'auto',
         },
-        looserCapture: {
+        winnerCapture: {
             border : '5px solid green',
             height : '5vw',
             width : 'auto',
-            marginBottom : '29.5vw',
-            marginLeft : '-12vh',
+            marginBottom : '26vw',
+            marginLeft : '-19vh',
         }
     }
 
@@ -24,13 +23,13 @@ const LastScreen = ({ winnerCapture, looserCapture, displayLastscreen }) => {
         <>
             {displayLastscreen &&
                 <div>
-                    <img
-                        style={styles.winnerCapture}
-                        src={winnerCapture} alt="winner"
-                    />
                     <img 
                         style={styles.looserCapture}
                         src={looserCapture} alt="looser" 
+                    />
+                     <img
+                        style={styles.winnerCapture}
+                        src={winnerCapture} alt="winner"
                     />
                 </div>
             }
